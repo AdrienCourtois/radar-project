7a464060da180f872196babee95e290794e6c802
adrien.courtois <adrien.courtois@telecom-paristech.fr>
2020-02-12 18:25:32 +0100

    Directly pass the pandas so that train test split is easier

--- utils/dataset.py
+++ utils/dataset.py
@@ -19,15 +19,14 @@ def gaussian_blur(x):
 class ImageDataset(Dataset):
     # Custom Dataset for the challenge #
 
-    def __init__(self, csv_file, root_dir, transform=True):
+    def __init__(self, csv, root_dir, transform=True):
         """
         Args:
-            csv_file (string): Path to the csv file with annotations.
+            csv (pandas.DataFrame): DataFrame where to find the path to the images.
             root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            transform (boolean, optional): Should the transform be applied?
         """
-        self.csv = pd.read_csv(csv_file)
+        self.csv = csv
         self.root_dir = root_dir
         self.transform = transform
 
