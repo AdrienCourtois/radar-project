10f7ee2eb0d4cc8437ef6e1d2cccefa3ccc91fcb
adrien.courtois <adrien.courtois@telecom-paristech.fr>
2020-03-08 19:47:11 +0100

    Modified the training to allow more flexibility for the batch size

--- train.py
+++ train.py
@@ -10,16 +10,14 @@ import pandas as pd
 import sys
 from sklearn.model_selection import train_test_split
 
-# GitHub imports
-sys.path.append('radar-project')
-
 from utils.dataset import ImageDataset
 from attention_unet import AttentionUNet
 
 # Dataset
-batch_size = 10
+batch_size = 1
+true_batch_size = 10
 
-dataset = ImageDataset("AOI_3_Paris_Train/RGB-normalized", "AOI_3_Paris_Train/segmentation", transform=False)
+dataset = ImageDataset("../AOI_3_Paris_Train/RGB-normalized", "../AOI_3_Paris_Train/segmentation", transform=False)
 
 # Train test split
 dataset_train, dataset_test = torch.utils.data.random_split(dataset, [int(0.8 * len(dataset)), len(dataset)-int(0.8 * len(dataset))])
@@ -44,7 +42,7 @@ def iou_loss(y_pred, y_true):
 def dice_score(y_pred, y_true):
     return 2 * ((y_pred * y_true).sum()) / (y_pred.sum() + y_true.sum())
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 for epoch in range(n_epochs):
 
@@ -52,31 +50,30 @@ for epoch in range(n_epochs):
     model.train()
 
     total_loss = 0
-    n_train = 0
 
     for idx, (x, y) in enumerate(train_loader):
         x, y = x.cuda(), y.cuda()
 
         y_pred = model(x)
         y_pred = torch.sigmoid(y_pred)
-
-        print(y_pred.sum())
         
         crossentropy = (-0.8 * y * torch.log(y_pred + 1e-8) - 0.2 * (1-y) * torch.log(1-y_pred + 1e-8)).mean()
         loss = iou_loss(y_pred, y) + crossentropy
         loss.backward()
 
-        optimizer.step()
-        optimizer.zero_grad()
+        if idx % true_batch_size == true_batch_size-1:
+            optimizer.step()
+            optimizer.zero_grad()
+    
+        if idx % 100 == 0 and idx > 0:
+            print("{} done on {} total.".format(idx, len(train_loader)))
 
         total_loss += loss.item()
-        n_train += 1
 
     # Evaluation
     model.eval()
 
     total_metric = 0
-    n_val = 0
 
     for x, y in test_loader:
         x, y = x.cuda(), y.cuda()
@@ -85,6 +82,5 @@ for epoch in range(n_epochs):
         y_pred = torch.sigmoid(y_pred)
 
         total_metric += dice_score(y_pred, y).item()
-        n_val += 1
     
-    print("Epoch {}, training loss {}, validation metric {}".format(epoch+1, total_loss / n_train, total_metric / n_val))
+    print("Epoch {}, training loss {}, validation metric {}".format(epoch+1, total_loss / len(train_loader), total_metric / len(test_loader)))
