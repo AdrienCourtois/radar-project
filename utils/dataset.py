ee67f7c9b7b5e490a58fcac8ce8a15aac9e96e62
adrien.courtois <adrien.courtois@telecom-paristech.fr>
2020-03-24 13:05:28 +0100

    Added comments and bug fix

--- utils/dataset.py
+++ utils/dataset.py
@@ -17,10 +17,10 @@ def gaussian_blur(x):
 
 
 class ImageDataset(Dataset):
-    # Custom Dataset for the challenge #
-
     def __init__(self, img_dir, label_dir, transform=True, height=512, width=512):
         """
+        Dataset class used for the project.
+
         Args:
             img_dir (string): Directory with all the images.
             label_dir (string): Directory with all the labels.
@@ -48,16 +48,23 @@ class ImageDataset(Dataset):
         self.n_images = len(self.image_names)
     
     def F_transform(self, image, mask):
+        """
+        Data augmentation when the parameter `transform` is set to True
+
+        Args:
+            image (PIL.Image array): The input image
+            mask (PIL.Image array): The input mask
+        """
+
         or_width, or_height = image.size
-        np_mask = np.array(mask)
 
         # RandomCrop
-        if np.random.rand() <= 1: # really random
+        if self.height < or_height or self.width < or_width:
             top = np.random.randint(or_height - self.height)
             left = np.random.randint(or_width - self.width)
-        
-        image = transforms.functional.crop(image, top, left, self.height, self.width)
-        mask = transforms.functional.crop(mask, top, left, self.height, self.width)
+            
+            image = transforms.functional.crop(image, top, left, self.height, self.width)
+            mask = transforms.functional.crop(mask, top, left, self.height, self.width)
 
         # Random rotations
         if np.random.rand() <= 0.5:
@@ -74,13 +81,61 @@ class ImageDataset(Dataset):
         if np.random.rand() <= 0.5:
             image = transforms.functional.vflip(image)
             mask = transforms.functional.vflip(mask)
+        
+        # Post transform
+        image = self.post_transform(image)
+        mask = transforms.ToTensor()(mask)
+
+        # Readjust the mask
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
+
+        return image, mask
+    
+    def F_noTransform(image, mask):
+        """
+        Data adjustement when the parameter `transform` is set to False.
+        e.g modifies the image size by resizing, adjusts the mean and std.
+
+        Args:
+            image (PIL.Image array): The input image
+            mask (PIL.Image array): The input mask
+        """
+
+        or_width, or_height = image.size
+
+        # Resize in case random crop is applied
+        if self.height < or_height or self.width < or_width:
+            image = transforms.functional.resize(image, (self.height, self.width), interpolation=3)
+            mask = transforms.functional.resize(mask, (self.height, self.width), interpolation=3)
+        
+        # Post transform
+        image = self.post_transform(image)
+        mask = transforms.ToTensor()(mask)
+
+        # Readjust the mask
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
 
         return image, mask
 
     def __len__(self):
+        """
+        Overwritten built-in Python method. Returns the number of images
+        in the dataset.
+        """
+
         return self.n_images
 
     def __getitem__(self, idx):
+        """
+        Overwritten built-in Python method. Returns the image #`idx`, 
+        possibly augmented, and its associated mask.
+
+        Args:
+            idx (number): The image ID, must be in [0, len(self)-1]
+        """
+
         img_name = os.path.join(self.img_dir, self.image_names[idx])
         image = Image.open(img_name)
 
@@ -90,12 +145,7 @@ class ImageDataset(Dataset):
 
         if self.transform:
             image, mask = self.F_transform(image, mask)
+        else:
 
-        image = self.post_transform(image)
-        mask = transforms.ToTensor()(mask)
-
-        # Readjust the mask
-        mask[mask > 0.5] = 1
-        mask[mask <= 0.5] = 0
 
         return image, mask
