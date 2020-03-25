570a71e8f32cf806c1f13ad1698a7e3bfa1f72f3
Kevin Mrc <43733329+KevinMrc@users.noreply.github.com>
2020-03-25 07:02:07 +0100

    Added load_chekpoint

--- utils/save.py
+++ utils/save.py
@@ -19,3 +19,20 @@ def save_checkpoint(state, is_best, save_path, filename='checkpoint.pth.tar'):
     if is_best:
         path2 = os.path.join(save_path, 'model_best.pth.tar')
         shutil.copyfile(path, path2)
+        
+def load_chekpoint(model, optimizer, resume):
+    if resume:
+        if os.path.isfile(resume):
+            print(f"Loading checkpoint: {resume}.")
+            checkpoint = torch.load(resume)
+            start_epoch = checkpoint['epoch']
+            best_IOU = checkpoint['best_IOU']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            cpt = checkpoint['iter']
+            print(f"Checkpoint loaded: Start epoch: {start_epoch}, Best IOU: {best_IOU:0.3f}.")
+        else:
+            print(f"No checkpoint found at: {resume}.")
+    else:
+      print(f"No checkpoint loaded.")
+    return model, optimizer, start_epoch, best_IOU, cpt 
