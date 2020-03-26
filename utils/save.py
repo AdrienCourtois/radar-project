dd5127653f681d966626401d6204d41a4fca7c37
Kevin Mrc <43733329+KevinMrc@users.noreply.github.com>
2020-03-26 02:46:47 +0100

    Added strict loader

--- utils/save.py
+++ utils/save.py
@@ -20,22 +20,22 @@ def save_checkpoint(state, is_best, save_path, filename='checkpoint.pth.tar'):
         path2 = os.path.join(save_path, 'model_best.pth.tar')
         shutil.copyfile(path, path2)
         
-def load_checkpoint(model, optimizer, resume):
+def load_checkpoint(model, optimizer, resume, strict=False):
+    start_epoch = 0
+    best_IOU = 0
+    cpt = 1
     if resume:
         if os.path.isfile(resume):
             print(f"Loading checkpoint: {resume}.")
             checkpoint = torch.load(resume)
             start_epoch = checkpoint['epoch']
             best_IOU = checkpoint['best_IOU']
-            model.load_state_dict(checkpoint['state_dict'])
+            model.load_state_dict(checkpoint['state_dict'], strict=strict)
             optimizer.load_state_dict(checkpoint['optimizer'])
             cpt = checkpoint['iter']
             print(f"Checkpoint loaded: Start epoch: {start_epoch}, Best IOU: {best_IOU:0.3f}.")
         else:
             print(f"No checkpoint found at: {resume}.")
     else:
-        start_epoch = 0
-        best_IOU = 0
-        cpt = 1
         print(f"No checkpoint loaded.")
     return model, optimizer, start_epoch, best_IOU, cpt 
