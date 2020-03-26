1c5d2d6d4b405d3975e0da4b67f7aa581e623b12
Kevin Mrc <43733329+KevinMrc@users.noreply.github.com>
2020-03-26 03:40:26 +0100

    Added mismatch loader

--- utils/save.py
+++ utils/save.py
@@ -20,7 +20,7 @@ def save_checkpoint(state, is_best, save_path, filename='checkpoint.pth.tar'):
         path2 = os.path.join(save_path, 'model_best.pth.tar')
         shutil.copyfile(path, path2)
         
-def load_checkpoint(model, optimizer, resume, strict=False):
+def load_checkpoint(model, optimizer, resume, mismatch=False):
     start_epoch = 0
     best_IOU = 0
     cpt = 1
@@ -30,7 +30,9 @@ def load_checkpoint(model, optimizer, resume, strict=False):
             checkpoint = torch.load(resume)
             start_epoch = checkpoint['epoch']
             best_IOU = checkpoint['best_IOU']
-            model.load_state_dict(checkpoint['state_dict'], strict=strict)
+            if mismatch:
+              del checkpoint['state_dict']['DownConv0.conv.0.weight']
+            model.load_state_dict(checkpoint['state_dict'], strict=False)
             optimizer.load_state_dict(checkpoint['optimizer'])
             cpt = checkpoint['iter']
             print(f"Checkpoint loaded: Start epoch: {start_epoch}, Best IOU: {best_IOU:0.3f}.")
